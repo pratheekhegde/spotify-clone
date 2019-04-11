1f5083d57f556215c6af4a740ca7507b3e497697
Pratheek Hegde <ptk609@gmail.com>
2019-04-12 00:16:26 +0530

    add hover play icon for media objects

--- src/views/browse/components/mediaObject/index.js
+++ src/views/browse/components/mediaObject/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import {MediaObjectContainer} from './style';
+import {MediaObjectContainer, MediaImage, PlayIcon} from './style';
 
 const MediaObject = props => 
     <MediaObjectContainer>
-        <img className="image" alt="" src={props.imageUrl} />
+        <MediaImage src={props.imageUrl} />
         <div className="title">{props.title}</div>
         <div className="subtitle">{props.subTitle}</div>
-        <div className="playIcon">
-            {/* <svg><path fill="currentColor" d="M81 44.6c5 3 5 7.8 0 10.8L9 98.7c-5 3-9 .7-9-5V6.3c0-5.7 4-8 9-5l72 43.3z"><title>PLAY</title></path></svg> */}
-        </div>
+        <PlayIcon>
+            <svg className="svg" viewBox="0 0 85 100"><path fill="currentColor" d="M81 44.6c5 3 5 7.8 0 10.8L9 98.7c-5 3-9 .7-9-5V6.3c0-5.7 4-8 9-5l72 43.3z"><title>PLAY</title></path></svg>
+        </PlayIcon>
     </MediaObjectContainer>
 
 export default MediaObject;
--- src/views/browse/components/mediaObject/style.js
+++ src/views/browse/components/mediaObject/style.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const MediaObjectContainer = styled.div`
+    position: relative;
     display: flex;
     flex-direction: column;
     max-width: 177px;
@@ -10,22 +11,6 @@ export const MediaObjectContainer = styled.div`
     transition: none 33ms cubic-bezier(.3,0,.7,1);
     transition-property: transform;
 
-    img {
-        cursor: pointer;
-        background-color: #282828;
-        max-width: 177px;
-        box-shadow: 0 0 10px rgba(0,0,0,.3);
-        transition-property: opacity;
-        transition-timing-function: cubic-bezier(.3,0,.4,1);
-        transition-duration: .1s;
-        &:hover{
-            opacity: 0.3;
-        }
-    }
-
-    .playIcon {
-        transform: scale(.5);
-    }
     .title {
         cursor: pointer;
         color: white;
@@ -40,3 +25,59 @@ export const MediaObjectContainer = styled.div`
         padding-top: 10px;
     }
 `
+
+export const MediaImage = styled.img`
+    cursor: pointer;
+    background-color: #282828;
+    max-width: 177px;
+    box-shadow: 0 0 10px rgba(0,0,0,.3);
+    transition-property: opacity;
+    transition-timing-function: cubic-bezier(.3,0,.4,1);
+    transition-duration: .1s;
+
+    ${MediaObjectContainer}:hover & {
+       opacity: 0.3;
+    }
+`;
+
+export const PlayIcon = styled.div`
+    background-color: rgba(0,0,0,0);
+    display: none;
+    position: absolute;
+    width: 62px;
+    height: 62px;
+    top: 22%;
+    left: 32%;
+    color: #fff;
+    border: 0;
+    border-radius: 500px;
+    box-sizing: content-box;
+    outline: none;
+    box-shadow: inset 0 0 0 1px #b3b3b3;
+    &::after{
+        content: "";
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        border-radius: 500px;
+        box-shadow: inset 0 0 0 1px #b3b3b3;
+    }
+
+    .svg {
+        margin-left: 4px;
+        transform: scale(.5);
+        width: 100%;
+        height: 100%;
+    }
+    &:hover {
+        transform: scale(1.06);
+        transition: none 33ms cubic-bezier(.3,0,0,1);
+        transition-property: all;
+        box-shadow: inset 0 0 0 1px #fff;
+    }
+    ${MediaObjectContainer}:hover & {
+        display: block;
+    }
+`;
